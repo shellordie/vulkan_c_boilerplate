c9fb9c2f64ceb74a34abcc05baa28e22e0e23a5e
shellordie <ctchanake@gmail.com>
2026-04-28 21:47:34 +0000

    woring on darray

--- core/darray.c
+++ core/darray.c
@@ -1,41 +1,67 @@
 #include "darray.h"
 
-void* _darray_allocate(uint32_t stride, uint32_t capacity)
+void* _darray_allocate(u64 stride, u64 capacity)
 {
-  uint64_t mem_size_to_allocate=(stride*capacity)+sizeof(header_t);
+  u64 mem_size_to_allocate=(stride*capacity)+header_size;
 
-  void* p_mem_address=mem_allocate(mem_size_to_allocate);
-
-  header_t header={stride,capacity,0};
+  u64* p_mem_address=(u64*)mem_allocate(mem_size_to_allocate);
 
   assert_failure(p_mem_address!=NULL,"mem_allocate failed");
 
-  p_mem_address=mem_copy(p_mem_address,(void*)&header,sizeof(header_t));
+  p_mem_address[HEADER_TYPE_STRIDE]=stride;
+  p_mem_address[HEADER_TYPE_CAPACITY]=capacity;
+  p_mem_address[HEADER_TYPE_USED]=0;
+
+  //p_mem_address=mem_copy(p_mem_address,(void*)&header,sizeof(header_t));
 
   //mem address to return
-  uint64_t* mem_address=(uint64_t*)p_mem_address;
-  *mem_address+=sizeof(uint32_t);
+  //uint64_t* mem_address=(uint64_t*)p_mem_address;
+  *p_mem_address+=header_size;
 
-  return (void*)mem_address;
+  return (void*)p_mem_address;
 }
 
 void _darray_destroy(void* p_mem_address)
 {
   // calculate real mem address
-  uint64_t* real_mem_address=(uint64_t*)p_mem_address;
-  *real_mem_address-=sizeof(header_t);
+  u64* real_mem_address=(u64*)p_mem_address;
+  *real_mem_address-=header_size;
   mem_free((void*)real_mem_address);
 }
 
+u64 _darray_get_info(void* p_mem_address,enum header_t header_type)
+{
+  // calculate real mem address
+  u64* p_real_mem_address=(u64*)p_mem_address;
+  *p_real_mem_address-=header_size;
+  u64 info;
+  switch(header_type)
+  {
+    case HEADER_TYPE_STRIDE:
+      info=p_real_mem_address[HEADER_TYPE_STRIDE];
+      break;
+    case HEADER_TYPE_CAPACITY:
+      info=p_real_mem_address[HEADER_TYPE_CAPACITY];
+      break;
+    case HEADER_TYPE_USED:
+      info=p_real_mem_address[HEADER_TYPE_USED];
+      break;
+    default:
+  }
+  return info;
+}
 
 void darray_test()
 {
   int* p_int_array=darray_create(int);
   assert(p_int_array!=NULL,"darray_create failed","array_allocated");
+  printf("array capacity =%d \n",darray_get_size(p_int_array));
+  p_int_array[0]=3;
   darray_destroy(p_int_array);
 
   p_int_array=darray_reserve(int,2);
   assert(p_int_array!=NULL,"darray_reserve failed","array_allocated");
+  printf("array capacity =%d \n",darray_get_size(p_int_array));
   darray_destroy(p_int_array);
 }
 
--- core/darray.h
+++ core/darray.h
@@ -2,21 +2,42 @@
 #define _DARRAY_H
 #include "../defines.h"
 
-typedef struct header_t 
+enum header_t 
 {
-  uint32_t stride;
-  uint32_t capacity;
-  uint32_t used;
-}header_t;
+  HEADER_TYPE_STRIDE=0,
+  HEADER_TYPE_CAPACITY=1,
+  HEADER_TYPE_USED=2
+};
 
-void* _darray_allocate(uint32_t stride, uint32_t capacity);
+#define header_size 3*sizeof(u64)
+
+void* _darray_allocate(u64 stride, u64 capacity);
 void _darray_destroy(void* p_mem_address);
+u64 _darray_get_info(void* p_mem_address,enum header_t header);
+void _darray_push(void* p_mem_address,u64 stride,void* p_data,u64 index);
 void darray_test();
 
 #define darray_create(type) _darray_allocate(sizeof(type),1)
 
 #define darray_reserve(type,capacity) _darray_allocate(sizeof(type),capacity)
 
-#define darray_destroy(mem_address) _darray_destroy((void*)mem_address)
+#define darray_destroy(p_mem_address) _darray_destroy((void*)p_mem_address)
+
+#define darray_get_size(p_mem_address) _darray_get_info((void*)p_mem_address,HEADER_TYPE_CAPACITY)
+#define darray_get_size_used(p_mem_address) _darray_get_info((void*)p_mem_address,HEADER_TYPE_USED)
+
+#define darray_push(p_mem_address,p_data)\
+{\
+  u64 stride=sizeof(typeof(p_data));\
+  u64 index=darray_get_size_used(p_mem_address)+1;\
+  _darray_push(p_mem_address,stride,p_data,index);\
+}\
+
+#define darray_push_at(p_mem_address,p_data,index)\
+{\
+  u64 stride=sizeof(typeof(p_data));\
+  _darray_push(p_mem_address,stride,p_data,index);\
+}\
+
 
 #endif
