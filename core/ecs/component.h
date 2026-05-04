89494f6a1b334979cce47a747a5f367f8d95558f
shellordie <ctchanake@gmail.com>
2026-05-04 20:14:36 +0000

    start working on ecs

---
+++ core/ecs/component.h
@@ -0,0 +1,11 @@
+#ifndef _COMPONENT_TYPE_H_
+#define _COMPONENT_TYPE_H_
+
+#include "ecs_type.h"
+
+typedef enum component_type_t
+{
+  COMPONENT_TYPE_LAST=(MAX_COMPONENT_TYPE-1)
+}component_type_t;
+
+#endif
---
+++ core/ecs/ecs_type.h
@@ -0,0 +1,32 @@
+#ifndef _ECS_TYPE_H_
+#define _ECS_TYPE_H_
+
+#include "defines.h"
+
+
+// entity type
+#define MAX_CHAR_LENGTH 250
+#define MAX_ENTITY 20000
+#define MAX_COMPONENT_TYPE 100
+
+typedef struct entity_t 
+{
+  u64 id;
+  char[MAX_CHAR_LENGTH] name;
+}entity_t;
+
+typedef struct enity_set_t
+{
+  entity_t* p_set;
+}entity_array_t;
+
+//components
+
+typedef struct components_set_t 
+{
+  //component_type_t available_component[MAX_COMPONENTS];
+  void* p_components_array[MAX_COMPONENT_TYPE];
+}components_set_t;
+
+
+#endif
