4618de6183e2983bce3d208d23e1cb9bf68adebc
shellordie <ctchanake@gmail.com>
2026-05-05 23:24:47 +0000

    some fix

--- core/ecs/entity.c
+++ core/ecs/entity.c
@@ -6,8 +6,8 @@ b8 entity_create(char* name,entities_t* p_entities)
   {
     printf("initializing srand\n");
     srand(1000000);
+    p_entities->p_array=(entity_t*)darray_reserve(entity_t,MAX_ENTITY);
   }
-  p_entities->p_array=(entity_t*)darray_reserve(entity_t,MAX_ENTITY);
 
   entity_t new_entity;
   new_entity.id=(u64)rand();
@@ -40,8 +40,9 @@ void entity_destroy(entities_t* p_entities)
 
 void entity_test()
 {
-  entities_t entity_world;
+  entities_t entity_world={NULL};
   entity_create("floor",&entity_world);
+  entity_create("triangle",&entity_world);
 
   for(u64 i=0;i<darray_get_used(entity_world.p_array);i++)
   {
@@ -50,6 +51,11 @@ void entity_test()
     printf("id= %llu\n",entity.id);
   }
   printf("array id =%llu\n",entity_get_id("floor",&entity_world));
+
+  printf("array capacity = %llu\n",darray_get_capacity(entity_world.p_array));
+  printf("array stride  =%llu\n",darray_get_stride(entity_world.p_array));
+  printf("array used=%llu\n",darray_get_used(entity_world.p_array));
+ 
   entity_destroy(&entity_world);
 
   assert_failure(1!=1,"break here for test");
